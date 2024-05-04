eb552184008f99cab1ddaa60b84e6a51962063ac
Annesa Tran <annesat2020@gmail.com>
2024-05-03 19:39:48 -0700

    Filled in following page

--- soundcrate/src/app/user/[username]/profile/following/page.js
+++ soundcrate/src/app/user/[username]/profile/following/page.js
@@ -1,7 +1,36 @@
+import { FollowUserCard } from '@/components';
+
 export default function UserFollowingPage({ params }) {
   const { username } = params;
+  
+  // mock data
+  const following_data = [
+    { 
+      username: "johndoe",
+      profile_img: null,
+      review_count: 10,
+      album_count: 3
+    },
+    { 
+      username: "musicluvr9",
+      profile_img: null,
+      review_count: 29,
+      album_count: 7
+    },
+  ];
+
+  const follow_user_cards = following_data.map((user, i) =>
+    <FollowUserCard
+      key={`follow-user-card-${i}`} 
+      user_data={user} />
+  );
 
   return (
-    <h1>UserFollowingPage for {username}</h1>
+    <main className="flex flex-col gap-4">
+      <h1>Following</h1>
+      <div className="flex flex-col gap-2">
+        {follow_user_cards}
+      </div>
+    </main>
   );
 }
