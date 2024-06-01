4ef264e5521d15586e86e76446f2b127d872787c
Annesa Tran <annesat2020@gmail.com>
2024-06-01 12:07:37 -0700

    Finished base of API to get array of reviews

---
+++ soundcrate/src/app/api/review/getReviews/route.js
@@ -0,0 +1,30 @@
+import { connectMongoDB } from '@/lib/mongodb';
+import Review from '@/lib/models/review';
+import { NextResponse } from 'next/server';
+
+export async function GET(req){
+  try {
+    await connectMongoDB();
+    const search_params = Object.fromEntries(req.nextUrl.searchParams.entries());
+
+    // initialize an object to filter reviews
+    let filter_query = {};
+    
+    // add in filters specified in search params
+    if ('userId' in search_params) { filter_query.user = search_params.userId };
+    if ('songId' in search_params) { filter_query.songId = search_params.songId };
+    
+    const reviews = await Review.find(filter_query);
+
+    return NextResponse.json(
+      { body: reviews }, 
+      { status: 200 }
+    )
+
+  } catch (error) {
+    return NextResponse.json(
+      { message: `Internal Server Error: ${error}` },
+      { status: 500 }
+    )
+  }
+}
