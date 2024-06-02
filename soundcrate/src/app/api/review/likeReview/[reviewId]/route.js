ba052817dfd5b1a985ca1604ba7c89c4b5a8a3dd
Annesa Tran <annesat2020@gmail.com>
2024-06-01 20:48:20 -0700

    Updated styling LikeButton component

---
+++ soundcrate/src/app/api/review/likeReview/[reviewId]/route.js
@@ -0,0 +1,57 @@
+import { connectMongoDB } from '@/lib/mongodb';
+import Review from '@/lib/models/review';
+// import User from '@/lib/models/user';
+import { NextResponse } from 'next/server';
+
+export async function POST(req, { params }){
+  try {
+    const review_id = params.reviewId;
+    const { user_id, action } = await req.json(); // TODO: retrieve user id from headers instead
+
+    await connectMongoDB();
+
+    var res = null;
+
+    let action_obj = {};
+    switch(action) {
+      case 'like':
+        action_obj = { $addToSet: { likes: user_id } };
+        break;
+      case 'unlike':
+        action_obj = { $pull: { likes: user_id } };action_obj = { $pull: { likes: user_id } }
+        break;
+      default:
+        return NextResponse.json(
+          { message: 'Missing "action" field' },
+          { status: 400 }
+        )
+    }
+
+    await Review.findOneAndUpdate(
+      { _id: review_id },
+      { ...action_obj },
+      { new: true }
+    ).then((doc) => {
+      res = NextResponse.json(
+        { body: 
+          {
+            like_count: (doc.likes).length,
+            is_liked: (doc.likes).includes(user_id)
+          }
+        }, 
+        { status: 200 }
+      )
+    })
+    .catch((error) => {
+      throw error;
+    });
+
+  } catch (error) {
+    res = NextResponse.json(
+      { message: "Internal Server Error: " + error },
+      { status: 500 }
+    )
+  } finally {
+    return res;
+  }
+}
--- soundcrate/src/components/SongReviewCard.js
+++ soundcrate/src/components/SongReviewCard.js
@@ -15,7 +15,8 @@ export default function SongReviewCard({
   image='',
   track_number=null,
   show_image=true,
-  detail_type='album'
+  detail_type='album',
+  likes=[]
   }) {
 
   // display either album art + song details OR profile image + username
@@ -78,7 +79,11 @@ export default function SongReviewCard({
         </div>
 
         {/* 2 - like count */}
-        <LikeButton review_id={review_id} />
+        <LikeButton 
+          review_id={review_id}
+          likes={likes}
+          compact={true}
+        />
       </div>
     </Link>
   );
