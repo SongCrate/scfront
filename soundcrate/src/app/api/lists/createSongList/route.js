39ae0494344f9af102d144d91b935d833fe3d5a1
jemeduria <julianneeduria23@gmail.com>
2024-06-05 15:24:34 -0700

    almost done with create list, running into Syntax Error with JSON

--- soundcrate/src/app/api/lists/createSongList/route.js
+++ soundcrate/src/app/api/lists/createSongList/route.js
@@ -1,26 +1,30 @@
 import { connectMongoDB } from '@/lib/mongodb';
+import User from '@/lib/models/user';
 import SongList from '@/lib/models/songList';
 import { NextResponse } from 'next/server';
 
 export async function POST(req) {
     try {
         // get user ID, title, and description
-        const { user_id, list_title, list_description } = await req.json();
+        const {user_id, list_title, list_description } = await req.json();
 
         // make sure that userId, title, and description are provided
         if (!user_id || !list_title || !list_description) {
             return NextResponse.json(
-                { message: 'user needs to input title and description' },
+                { message: user_id + ", " + list_title + ", " + list_description },
                 { status: 400 }
             );
         }
 
         await connectMongoDB();
 
+        // const username = req.nextUrl.searchParams.get('username');
+        // const user = await User.findOne({ username })
+
         // create document
         var songlist = new SongList({
             user: user_id,
-            title: listlist_title,
+            title: list_title,
             desciption: list_description,
             songIds: [],
         });
@@ -31,21 +35,31 @@ export async function POST(req) {
         await SongList.create(songlist).then((doc) => {
             res = NextResponse.json(
                 { body: doc },
-                { status: 200}
+                { status: 200 }
             )
         }).catch((error) => {
-            throw error;
+            console.log(error);
         })
 
         // save the new song list to the database (???)
-        // await songlist.save();
+        // const songlist = new SongList({
+        //     user: user._id,
+        //     title: list_title,
+        //     description: list_description,
+        //     songIds: [],
+        // });
+
+        // const doc = await songlist.save();
+
+        // return NextResponse.json({ body: doc }, { status: 200 });
 
     } catch (error) {
         return NextResponse.json(
             { message: "Internal Server Error: " + error },
             { status: 500 }
         ); 
-    } finally {
-        return res;
-    }
+    } 
+    // finally {
+    //     return res;
+    // }
 }
--- soundcrate/src/components/CreateListModal.js
+++ soundcrate/src/components/CreateListModal.js
@@ -1,54 +1,62 @@
 'use client';
-import { get_db } from '/utils';
 import { useState } from 'react';
-import { 
-  Plus,
-  X } from '@phosphor-icons/react';
+import { Plus, X } from '@phosphor-icons/react';
+import { useSession } from "next-auth/react";
 
 export default function CreateListModal() {
 
-  // mock data, would be grabbing this from header
-  const user_id = 1;
+  const { data: session } = useSession();
 
   const modal_id = "create-modal-id";
-  const [ name, setName ] = useState("");
+  const [ title, setName ] = useState("");
   const [ description, setDescription ] = useState("");
   const description_char_limit = 150;
-  const name_char_limit = 30;
+  const title_char_limit = 30;
 
-  const db = get_db();
+  const handleCancel = () => {
+    setName("");
+    setDescription("");
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    // create new database object
-    const last_id = db['list'].slice(-1)[0].id
-    var new_db = db
-    new_db['list'][last_id] = {
-      "id": last_id + 1,
-      "user_id": user_id,
-      "name": name.trim(),
-      "description": description.trim()
+
+    // // get user id from session
+    if (!session || !session.user) {
+      console.log("User not authenticated");
+      return;
     }
+    const user_id = session.user._id;
+
+    // create body to send to the API
+    const new_songlist = {
+      user_id: user_id,
+      list_title: title,
+      list_description: description,
+    };
 
     try {
-      console.log(new_db);
-      const response = await fetch('/api/update_db', {
+      // post to database
+      const response = await fetch('/api/lists/createSongList', {
         method: 'POST',
-        body: JSON.stringify(new_db)
+        body: JSON.stringify(new_songlist)
       });
 
       const response_data = await response.json();
 
-      if (response.status === 200) {
-        console.log('success');
+      if (response_data?.status == 200) {
+        console.log(response_data.body)
+        setTitle("");
+        setDescription("");
+        document.getElementById(modal_id).classList.add('hidden');
       } else {
-        console.log(response.status)
+        console.log(response_data.error)
       }
-    } catch (error) {
-      console.log(error);
-    }
 
+      } catch (error) {
+        console.log(error);
+      } 
+      e.preventDefault();
   }
 
   const renderForm = () => {
@@ -63,15 +71,15 @@ export default function CreateListModal() {
             <label htmlFor="list-name-input" className="block text-sm font-medium mb-1 text-light">
                 Name
             </label>
-            <span className={`block mb-2 text-sm opacity-60" ${name.length < name_char_limit - 10 ? 'invisible' : 'visible'}`}>
-              {name.length} / {name_char_limit}
+            <span className={`block mb-2 text-sm opacity-60" ${title.length < title_char_limit - 10 ? 'invisible' : 'visible'}`}>
+              {title.length} / {title_char_limit}
             </span>
           </div>
 
           <input 
             id="list-name-input"
-            maxLength={name_char_limit}
-            value={name}
+            maxLength={title_char_limit}
+            value={title}
             onChange={(e) => { setName(e.target.value) }}
           />
         </div>
@@ -99,12 +107,16 @@ export default function CreateListModal() {
 
         {/* cancel and action buttons */}
         <div className="flex justify-end items-center gap-x-2 p-3">
-          <button type="button" className="btn hs-dropup-toggle gap-x-2 text-sm font-medium rounded-md text-gray hover:bg-dark hover:bg-opacity-40" data-hs-overlay={"#"+modal_id}>
+          <button 
+            type="button" 
+            onClick={handleCancel}
+            className="btn hs-dropup-toggle gap-x-2 text-sm font-medium rounded-md text-gray hover:bg-dark hover:bg-opacity-40" 
+            data-hs-overlay={"#"+modal_id}>
             Cancel
           </button>
           <button 
             // type="submit"
-            disabled={!name}
+            disabled={!title}
             onClick={handleSubmit} 
             className="btn gap-x-2 text-sm rounded-md bg-blue hover:bg-opacity-80 text-light disabled:opacity-50"
           >
@@ -148,3 +160,161 @@ export default function CreateListModal() {
     </>
   );
 }
+
+
+
+
+
+
+
+
+// 'use client';
+// import { get_db } from '/utils';
+// import { useState } from 'react';
+// import { 
+//   Plus,
+//   X } from '@phosphor-icons/react';
+
+// export default function CreateListModal() {
+
+//   // mock data, would be grabbing this from header
+//   const user_id = 1;
+
+//   const modal_id = "create-modal-id";
+//   const [ name, setName ] = useState("");
+//   const [ description, setDescription ] = useState("");
+//   const description_char_limit = 150;
+//   const name_char_limit = 30;
+
+//   const db = get_db();
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+    
+//     // create new database object
+//     const last_id = db['list'].slice(-1)[0].id
+//     var new_db = db
+//     new_db['list'][last_id] = {
+//       "id": last_id + 1,
+//       "user_id": user_id,
+//       "name": name.trim(),
+//       "description": description.trim()
+//     }
+
+//     try {
+//       console.log(new_db);
+//       const response = await fetch('/api/update_db', {
+//         method: 'POST',
+//         body: JSON.stringify(new_db)
+//       });
+
+//       const response_data = await response.json();
+
+//       if (response.status === 200) {
+//         console.log('success');
+//       } else {
+//         console.log(response.status)
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+
+//   }
+
+//   const renderForm = () => {
+//     return (
+//       <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+
+//         {/* list name */}
+//         <div className="w-full">
+//           {/* <label htmlFor="list-name-input" className="block text-sm font-medium mb-1 text-light">Name</label> */}
+
+//           <div className="flex justify-between items-center">
+//             <label htmlFor="list-name-input" className="block text-sm font-medium mb-1 text-light">
+//                 Name
+//             </label>
+//             <span className={`block mb-2 text-sm opacity-60" ${name.length < name_char_limit - 10 ? 'invisible' : 'visible'}`}>
+//               {name.length} / {name_char_limit}
+//             </span>
+//           </div>
+
+//           <input 
+//             id="list-name-input"
+//             maxLength={name_char_limit}
+//             value={name}
+//             onChange={(e) => { setName(e.target.value) }}
+//           />
+//         </div>
+
+//         {/* list description */}
+//         <div className="w-full">
+//           <div className="flex justify-between items-center">
+//             <label htmlFor="list-desc-textarea" className="block text-sm font-medium mb-1 text-light">
+//               Description <span className="opacity-60">(Optional)</span>
+//             </label>
+//             <span className={`block mb-2 text-sm opacity-60" ${description.length < description_char_limit - 25 ? 'invisible' : 'visible'}`}>
+//               {description.length} / {description_char_limit}
+//             </span>
+//           </div>
+//           <textarea 
+//             id="list-desc-textarea" 
+//             className="py-2 px-3 block w-full bg-gray-dark rounded-md text-sm focus:border-gray focus:ring-gray disabled:opacity-50 disabled:pointer-events-none" 
+//             rows="3"
+//             maxLength={description_char_limit}
+//             value={description}
+//             onChange={(e) => { setDescription(e.target.value) }}
+//           >
+//           </textarea>
+//         </div>
+
+//         {/* cancel and action buttons */}
+//         <div className="flex justify-end items-center gap-x-2 p-3">
+//           <button type="button" className="btn hs-dropup-toggle gap-x-2 text-sm font-medium rounded-md text-gray hover:bg-dark hover:bg-opacity-40" data-hs-overlay={"#"+modal_id}>
+//             Cancel
+//           </button>
+//           <button 
+//             // type="submit"
+//             disabled={!name}
+//             onClick={handleSubmit} 
+//             className="btn gap-x-2 text-sm rounded-md bg-blue hover:bg-opacity-80 text-light disabled:opacity-50"
+//           >
+//             Create
+//           </button>
+//         </div>
+
+//       </form>
+//     )
+//   }
+
+//   return (
+//     <>
+//       <button type="button" className="btn p-2 bg-dark-light text-white rounded-md hover:bg-blue" data-hs-overlay={"#"+modal_id}>
+//         <Plus weight="bold" className="mr-1" /> New
+//       </button>
+
+//       <div id={modal_id} className="hs-overlay hs-overlay-backdrop-open:bg-dark-dark/80 hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none">
+//         {/* hs overlay */}
+//         <div className="opacity-100 transition-all hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-14 ease-out sm:max-w-lg sm:w-full m-3 sm:mx-auto">
+//           {/* modal box */}
+//           <div className="bg-dark-light flex flex-col shadow-sm rounded-lg pointer-events-auto">
+            
+//             {/* header and close button */}
+//             <div className="flex justify-between items-center py-3 px-4">
+//               <h3 className="font-bold">Create New List</h3>
+//               <button type="button" className="btn-round hs-dropup-toggle flex text-gray hover:bg-dark hover:bg-opacity-40" data-hs-overlay={"#"+modal_id}>
+//                 <span className="sr-only">Close</span>
+//                 <X size={18} />
+//               </button>
+//             </div>
+
+//             {/* modal body */}
+//             <div className="px-4 py-1">
+//               {renderForm()}
+//             </div>
+
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
