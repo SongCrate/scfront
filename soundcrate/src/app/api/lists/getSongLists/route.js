c6ab4958d74644e6d7a155f1fad8deaaa1f9f248
jemeduria <julianneeduria23@gmail.com>
2024-06-07 16:19:22 -0700

    lists vary by whose profile is being viewed

--- soundcrate/src/app/api/lists/getSongLists/route.js
+++ soundcrate/src/app/api/lists/getSongLists/route.js
@@ -7,12 +7,36 @@ export async function GET(req) {
     try {
         await connectMongoDB();
 
-        // find songlists based on a user
-        const user_id = req.headers.get('user_id');
+        // find songlists based on the profile NOT user logged in
+        // const user_id = req.headers.get('user_id');
+        
+        // Extract the username from the query parameters
+        const { searchParams } = new URL(req.url);
+        const username = searchParams.get('username');
+
+        if (!username) {
+            return NextResponse.json(
+                { message: "Username is required" },
+                { status: 400 }
+            );
+        }
+
+        // Find the user by username
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return NextResponse.json(
+                { message: "User not found" },
+                { status: 404 }
+            );
+        }
+
+
+
 
         // get all songlists of such user
         const song_lists = await SongList
-            .find({ user: user_id })
+            .find({ user: user._id })
             .populate('user')
             .exec();
         
